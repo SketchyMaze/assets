1eb144d89f9511d3e959c03f156f4c36c321af36
Noah Petherbridge <root@kirsle.net>
2021-12-26 20:28:06 -0800

    Add levelpacks

---
+++ levelpacks/build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# NOTE: `doodad` should be on your $PATH.
+# Run this script from its folder.
+
+AUTHOR="Noah P"
+
+# Tutorial Levelpack
+doodad levelpack create -t Tutorial -d "Learn how to play the game." \
+	-a "$AUTHOR" --doodads none \
+	"levelpacks/000000-TUTORIAL.levelpack" \
+	"../levels/Tutorial 1.level" \
+	"../levels/Tutorial 2.level" \
+	"../levels/Tutorial 3.level"
+
+# First Quest
+doodad levelpack create -t "First Quest" -d "The first story mode campaign." \
+	-a "$AUTHOR" --doodads none \
+	"levelpacks/000001-FIRST-QUEST.levelpack" \
+	"./src/FirstQuest/Boat.level" \
+	"./src/FirstQuest/Castle.level" \
+	"./src/FirstQuest/Desert-1of2.level" \
+	"./src/FirstQuest/Desert-2of2.level" \
+	"./src/FirstQuest/Thief 1.level"
